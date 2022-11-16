a711fcd8fcee69fb3f1b2e21644f3da2549b6c0a
Len Washington <l.washingtoniii.27@gmail.com>
2022-11-15 23:02:22 -0600

    Signup page is complete essentially complete. Included autocompletes to help users fill it in.

--- public_html/signup.php
+++ public_html/signup.php
@@ -80,19 +80,23 @@ while($row = pg_fetch_array($result)){
 
 <form name="signup_form" action="/api/signup" method="POST" onsubmit="return validate()">
 	<label for="username">Username: </label>
-	<input type="text" id="username" name="username" value="" required><br>
+	<input type="text" id="username" name="username" value="" required autocomplete="username"><br>
 
 	<label for="password">Password: </label>
-	<input type="password" id="password" name="password" value="" required><br>
+	<input type="password" id="password" name="password" value="" required autocomplete="new-password"><br>
 
 	<label for="fullname">Fullname: </label>
-	<input type="text" id="fullname" name="fullname" value="" required><br>
+	<input type="text" id="fullname" name="fullname" value="" required autocomplete="name"><br>
 
 	<label for="email">Email: </label>
-	<input type="text" id="email" name="email" value="" required><br>
+	<input type="email" id="email" name="email" value="" required autocomplete="email" pattern="^(([^<>()\[\]\\.,;:\s@]+(\.[^<>()\[\]\\.,;:\s@]+)*)|(.+))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$"><br>
+
+	<label for="phone">Telephone Number:</label>
+	<input name="phone" id="phone" value="" type="tel" required autocomplete="tel" pattern="\([0-9]{3}\)?-?[0-9]{3}-?[0-9]{4}"><br>
 
 	<label for="address_number">Address: </label>
-	<input type="number" id="address_number" name="address_number" value="" required>
+	<input type="number" id="address_number" name="address_number" placeholder="3301" required>
+
 	<select name="direction" id="direction">
 		<option value="None"></option>
 		<option value="N">North</option>
@@ -100,12 +104,13 @@ while($row = pg_fetch_array($result)){
 		<option value="S">South</option>
 		<option value="W">West</option>
 	</select>
-	<input type="text" name="streetname" id="streetname" value="" required>,
-	<input type="text" name="city" id="city" value="" required>,
-	<input type="text" name="state" id="state" value="" required maxlength="2">,
-	<input type="number" name="zipcode" id="zipcode" value="" required min="10000" max="99999">
+	<input type="text" name="streetname" id="streetname" placeholder="State" required>,
+	<input type="text" name="city" id="city" placeholder="Chicago" required>,
+	<input type="text" name="state" id="state" placeholder="IL" required maxlength="2">,
+	<input type="number" name="zipcode" id="zipcode" placeholder="60616" required min="10000" max="99999" autocomplete="postal-code"><br>
+	<label for="apt">Apt/Unit: </label><input type="text" name="apt" id="apt" value=""><br>
 
-	<label for="branch">Your favorite (or closest) branch:</label><select name="branch" id="branch" required>
+	<label for="branch">Your favorite (or closest) branch: </label><select name="branch" id="branch" required>
 		<?php foreach($dct as $key => $value) { ?>
 			<option value="<?php echo $key?>"><?php echo $value ?></option>
 		<?php }; ?>
