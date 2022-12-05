cc21fead3783c6127ddf59639c476ba7b2ae3d0c
Len Washington <l.washingtoniii.27@gmail.com>
2022-12-04 18:34:34 -0600

    Testing the side panel for Account Transactions in /profile.

--- public_html/profile.php
+++ public_html/profile.php
@@ -26,6 +26,7 @@ $loans = $user->getLoans();
 	<title>WCS Banking</title>
 	<link href="/css/menu_style.css" type="text/css" rel="stylesheet"/>
 	<link href="/css/wcss.php" type="text/css" rel="stylesheet"/>
+	<link href="/css/sidebar.css" type="text/css" rel="stylesheet"/>
 	<link rel="icon" type="image/x-icon" href="<?php echo FAVICON_LINK; ?>"/>
 	<script type="text/javascript">
 		function sidebar () {
@@ -61,95 +62,99 @@ $loans = $user->getLoans();
 	</script>
 </head>
 <body>
-<div id="content">
-	<nav id="page-side">
-		<div id="leftcontent">
-			<div id="account-number" class="side-account-info">
-				<div class="label">Account Number:</div><div id="number">The account's number</div>
-			</div>
-			<div id="account-name" class="side-account-info">
-				<div class="label">Name: </div><div id="name">The account's name</div> <!-- Should be clickable to change -->
-			</div>
-			<div id="account-balance" class="side-account-info">
-				<div class="label">Balance: </div><div id="balance">The account's balance</div> <!--The rest of these should just be displayed.-->
-			</div>
-			<div id="account-interest" class="side-account-info">
-				<div class="label">Interest: </div><div id="interest">The account's interest</div>
-			</div>
-			<div id="account-monthly-fees" class="side-account-info">
-				<div class="label">Monthly Fees: </div><div id="monthly_fees">The account's monthly fees</div>
-			</div>
-			<div id="account-overdrawn" class="side-account-info">
-				<div class="label">Overdrawn: </div><div id="overdrawn">If the account can be overdrawn.</div>
-			</div>
+<nav id="page-side">
+	<div id="leftcontent">
+		<div id="account-number" class="side-account-info">
+			<div class="label">Account Number:</div><div id="number">The account's number</div>
 		</div>
-		<div id="rightcontent">
-			<input type="text" id="transaction" name="transaction" pattern="Withdrawal|Deposit|Transfer" list="transactions" placeholder="Transaction Type" onchange="checkTransactionType()"><br>
-			<datalist id="transactions">
-				<option>Withdrawal</option>
-				<option>Deposit</option>
-				<option>Transfer</option>
-			</datalist>
-			$<input name="amount" id="amount" step="0.01" min="0" max="500" placeholder="Amount" required><br>
-			<input name="transfer_to_account_number" id="transfer_to_account_number" placeholder="Recipient Account Number" hidden>
+		<div id="account-name" class="side-account-info">
+			<div class="label">Name: </div><div id="name">The account's name</div> <!-- Should be clickable to change -->
 		</div>
-	</nav>
-	<main id="page-main">
-		<h2>My Accounts</h2>
-		<table id="accounts" class="profile_info">
-			<tr>
-				<th>Account Name</th>
-				<th>Balance</th>
-				<th>Type</th>
-				<th>Interest</th>
-				<th>Monthly Fee</th>
-				<th>Can Be Overdrawn</th>
-			</tr>
-			<?php if(is_array($accounts)) {foreach($accounts as $account) { ?>
-				<tr>
-					<td><?php echo $account->getName(); ?></td>
-					<td>$<?php echo sprintf("%.2f", $account->getBalance()); ?></td>
-					<td><?php echo $account->getType(); ?></td>
-					<td><?php echo $account->getInterest(); ?>%</td>
-					<td>$<?php echo sprintf("%.2f", $account->getMonthlyFee()); ?></td>
-					<td><?php if($account->canGoNegative()) { echo "True"; } else{ echo "False";} ?></td>
-				</tr>
-			<?php }}; ?>
-		</table>
-		<h2>My Loans</h2>
-		<table id="loans" class="profile_info">
-			<tr>
-				<th>Loan Name</th>
-				<th>Initial Amount</th>
-				<th>Remaining Amount</th>
-				<th>APR</th>
-			</tr>
-			<?php if(is_array($loans)){ foreach($loans as $loan) { ?>
-				<tr>
-					<td><?php echo $loan->getName(); ?></td>
-					<td>$<?php echo sprintf("%.2f", $loan->getInitialAmount()); ?></td> <!-- TODO: Make these right aligned on the period -->
-					<td>$<?php echo sprintf("%.2f", $loan->getAmountRemaining()); ?></td>
-					<td><?php echo $loan->getAPR(); ?>%</td>
-				</tr>
-			<?php }}; ?>
-		</table>
-		<nav class="floating-menu">
-			<?php if(!$db->isLoggedIn()): ?>
-				<h3>We sold you?</h3>
-				<a href="/login">Log In</a>
-				<a href="/signup">Sign Up</a>
-			<?php else: ?>
-				<h3>Hello <?php try {
-						echo $user->getFirstName();
-					} catch (PGException $e) {
-						echo "Internal Server Error";
-					} ?></h3>
-				<a href="/profile">Check My Profile</a>
-				<a href="/api/logout">Logout</a>
-			<?php endif; ?>
+		<div id="account-balance" class="side-account-info">
+			<div class="label">Balance: </div><div id="balance">The account's balance</div> <!--The rest of these should just be displayed.-->
+		</div>
+		<div id="account-interest" class="side-account-info">
+			<div class="label">Interest: </div><div id="interest">The account's interest</div>
+		</div>
+		<div id="account-monthly-fees" class="side-account-info">
+			<div class="label">Monthly Fees: </div><div id="monthly_fees">The account's monthly fees</div>
+		</div>
+		<div id="account-overdrawn" class="side-account-info">
+			<div class="label">Overdrawn: </div><div id="overdrawn">If the account can be overdrawn.</div>
+		</div>
+	</div>
+	<div id="rightcontent">
+		<input type="text" id="transaction" name="transaction" pattern="Withdrawal|Deposit|Transfer" list="transactions" placeholder="Transaction Type" onchange="checkTransactionType()"><br>
+		<datalist id="transactions">
+			<option>Withdrawal</option>
+			<option>Deposit</option>
+			<option>Transfer</option>
+		</datalist>
+		$<input name="amount" id="amount" step="0.01" min="0" max="500" placeholder="Amount" required><br>
+		<input name="transfer_to_account_number" id="transfer_to_account_number" placeholder="Recipient Account Number" hidden>
+	</div>
+</nav>
+<div id="content" class="page-main">
+	<button id="side-button" onclick="sidebar()">
+		&#9776;
+	</button>
 
-		</nav>
+	<!-- (B2) CONTENTS -->
+	<p>Contents</p>
+	<h2>My Accounts</h2>
+	<table id="accounts" class="profile_info">
+		<tr>
+			<th>Account Name</th>
+			<th>Balance</th>
+			<th>Type</th>
+			<th>Interest</th>
+			<th>Monthly Fee</th>
+			<th>Can Be Overdrawn</th>
+		</tr>
+		<?php if(is_array($accounts)) {foreach($accounts as $account) { ?>
+			<tr>
+				<td><?php echo $account->getName(); ?></td>
+				<td>$<?php echo sprintf("%.2f", $account->getBalance()); ?></td>
+				<td><?php echo $account->getType(); ?></td>
+				<td><?php echo $account->getInterest(); ?>%</td>
+				<td>$<?php echo sprintf("%.2f", $account->getMonthlyFee()); ?></td>
+				<td><?php if($account->canGoNegative()) { echo "True"; } else{ echo "False";} ?></td>
+			</tr>
+		<?php }}; ?>
+	</table>
+	<h2>My Loans</h2>
+	<table id="loans" class="profile_info">
+		<tr>
+			<th>Loan Name</th>
+			<th>Initial Amount</th>
+			<th>Remaining Amount</th>
+			<th>APR</th>
+		</tr>
+		<?php if(is_array($loans)){ foreach($loans as $loan) { ?>
+			<tr>
+				<td><?php echo $loan->getName(); ?></td>
+				<td>$<?php echo sprintf("%.2f", $loan->getInitialAmount()); ?></td> <!-- TODO: Make these right aligned on the period -->
+				<td>$<?php echo sprintf("%.2f", $loan->getAmountRemaining()); ?></td>
+				<td><?php echo $loan->getAPR(); ?>%</td>
+			</tr>
+		<?php }}; ?>
+	</table>
+	<nav class="floating-menu">
+		<?php if(!$db->isLoggedIn()): ?>
+			<h3>We sold you?</h3>
+			<a href="/login">Log In</a>
+			<a href="/signup">Sign Up</a>
+		<?php else: ?>
+			<h3>Hello <?php try {
+					echo $user->getFirstName();
+				} catch (PGException $e) {
+					echo "Internal Server Error";
+				} ?></h3>
+			<a href="/profile">Check My Profile</a>
+			<a href="/api/logout">Logout</a>
+		<?php endif; ?>
+
+	</nav>
 </div>
-	</main
 </body>
 </html>
