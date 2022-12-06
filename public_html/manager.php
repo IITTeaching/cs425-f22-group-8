266ee1784a9c0687a099cb2e48fe2ba141e81127
Rajan Savani <110261027+rajansavani@users.noreply.github.com>
2022-12-06 16:37:54 -0600

    background of modal box larger

--- public_html/manager.php
+++ public_html/manager.php
@@ -75,7 +75,7 @@ button:hover {
   background-color: #F0F0F0;
   margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
   border: 1px solid #888;
-  width: 80%; /* Could be more or less, depending on screen size */
+  width: 85%; /* Could be more or less, depending on screen size */
 }
 
 /* Style the horizontal ruler */
