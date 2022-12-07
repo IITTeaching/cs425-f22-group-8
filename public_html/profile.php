1c2441da3bef6fb1e5e3d7b5025f817a50732272
Len Washington <l.washingtoniii.27@gmail.com>
2022-12-07 14:57:09 -0600

    Everything for the user is now complete.

--- public_html/profile.php
+++ public_html/profile.php
@@ -128,7 +128,6 @@ $loans = $user->getLoans();
 		</tr>
 	</table>
 	<button class="addMore" title="To edit your account, click on one of your accounts to see the options.">?</button>
-	</div>
 	<div id="id01" class="modal">
 		<span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
 		<form class="modal-content" id="create_account_form">
