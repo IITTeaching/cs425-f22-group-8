814de118107d2c617e94de480e17e2c980d2d9cc
Rajan Savani <110261027+rajansavani@users.noreply.github.com>
2022-12-07 16:17:49 -0600

    changed path for wcs logo

--- public_html/index.php
+++ public_html/index.php
@@ -58,7 +58,7 @@ try{
 <div class="w3-top">
   <div class="w3-bar w3-white w3-card" id="myNavbar">
     <a href="/index" class="w3-bar-item w3-button w3-wide">
-		<img src = logos\wcs.png>
+		<img src = "/images/wcd.png" alt = "WCS">
 	</a>
   </div>
 </div>
