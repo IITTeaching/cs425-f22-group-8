204edb92272525404984d090b9ebe35206f21caa
Len Washington <l.washingtoniii.27@gmail.com>
2022-12-07 17:00:53 -0600

    Small change.

--- public_html/index.php
+++ public_html/index.php
@@ -68,7 +68,7 @@ try{
 <!-- Navbar (sit on top) -->
 <div class="w3-top">
   <div class="w3-bar w3-white w3-card" id="myNavbar">
-    <a href="/index" class="w3-bar-item w3-button w3-wide">
+    <a href="/" class="w3-bar-item w3-button w3-wide">
 		<img class = "img-nav" src = "/images/wcs.png" alt = "WCS">
 	</a>
   </div>
